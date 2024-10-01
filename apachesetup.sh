ba7d10acb5ea35e9483862c962d7cae1a3f15c01
fgdamasceno <fgdamasceno@gmail.com>
2024-10-01 15:45:57 -0300

    Script para instalar e configurar um servidor apache2

---
+++ apachesetup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#===================================================================================
+# DIO - FORMAÇÃO LINUX FUNDAMENTALS
+# Desafio de código
+#
+# - DEFINIÇÕES
+# * Atualizar o servidor;
+# * Instalar o apache2;
+# * Instalar o unzip;
+# * Baixar a aplicação disponível no endereço:
+#       https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+# * Copiar os arquivos da aplicação no diretório padrão do apache;
+#       /var/www/html/
+# * Subir arquivo de script para um repositório no GitHub;
+#
+# Author: F. G. Damasceno
+# Date: October 1, 2024
+#===================================================================================
+
+echo "ATUALIZANDO O SERVIDOR..."
+apt update
+apt upgrade -y
+
+echo "INSTALANDO APLICAÇÕES NECESSÁRIAS (apache2 e unzip)..."
+apt install apache2 unzip -y
+
+cd /tmp
+
+echo "BAIXANDO APLICAÇÃO WEB..."
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+
+mv linux-site-dio-main/ /var/www/html/
+
+echo "PROGRAMA FINALIZADO!"
+date
+echo ""
